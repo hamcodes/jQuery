3d4a077bd46098eba88b1d2f5345e4d20432b7f8
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2020-12-02 21:39:03 -0500

    creating functions in index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-$("h1").css("color", "red");
+$("h1").click(function() {
+  $("h1").css("color", "purple");
+});
+
+$(document).keydown(function(event) {
+  $("h1").text(event.key);
+});
+
+$("h1").on("mouseover", function() {
+  $("h1").css("color", "purple");
+});
+
