fa3da5787930d845d45e5bb1e354f9e233dc017b
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2020-12-02 21:49:05 -0500

    creating a function to hide the h1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $(document).keydown(function(event) {
   $("h1").text(event.key);
 });
 
-$("h1").on("mouseover", function() {
-  $("h1").css("color", "purple");
+$("h1").on("click", function() {
+  $("h1").hide();
 });
 
